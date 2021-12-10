39f0f8e944d8a9dc229f844181b38aeea47b7701
Sergei <urievkan@gmail.com>
2021-12-11 02:18:24 +0300

    Сделал базовую файловую структуру, сверстал большую часть шапки, начал верстать UI Kit

---
+++ src/Website/pages/roomsList/roomsList.js
@@ -0,0 +1,1 @@
+import './roomsList.scss'
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,32 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const PATHS = {
-    source:  path.join(__dirname, '/src'),
-    dist:  path.join(__dirname, '/dist'),
-    pages: path.join(__dirname, '/src/pug/pages')
+    source:  path.join(__dirname, '/src/'),
+    dist:  path.join(__dirname, '/dist/'),
+    pages: path.join(__dirname, '/src/Website/pages/'),
+    uikit: path.join(__dirname, '/src/UIKit/pages/')
 }
+
+//TODO: Исправить кавычки
 
 module.exports = {
     mode: "development",
+    entry: {
+        formElements:  PATHS.uikit + '/formElements/formElements.js',
+        index: PATHS.pages + '/index/index.js',
+        registration: PATHS.pages + '/registration/registration.js',
+        roomInfo: PATHS.pages + '/roomInfo/roomInfo.js',
+        roomsList: PATHS.pages + '/roomsList/roomsList.js'
+    },
     output: {
         path: PATHS.dist,
-        filename: 'index.js'
+        filename: '[name]/[name].js'
     },
-
-    plugins: [new HtmlWebpackPlugin({
-        filename: 'index.html',
-       // chunks: ['index'],
-        template: PATHS.pages + '/index/index.pug'
-    })],
+    devServer: {
+        static: [{directory: path.join(__dirname, 'dist')}, {directory: path.join(__dirname, 'src')}]
+    },
 
     module: {
         rules: [
@@ -31,8 +39,54 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: ["style-loader", "css-loader", "sass-loader"]
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.(ttf|woff|woff2|svg)$/,
+                use: [
+                        {
+                            loader: "file-loader",
+                            options: {
+                                name: '[name].[ext]',
+                                outputPath: 'fonts/'
+                            }
+                        }
+                    ]
             }
         ]
-    }
+    },
+    plugins: [new HtmlWebpackPlugin(
+        {
+            filename: 'index/index.html',
+            chunks: ['index'],
+            template: PATHS.pages + 'index/index.pug'
+        }),
+        new HtmlWebpackPlugin(
+            {
+                filename: 'formElements/formElements.html',
+                chunks: ['formElements'],
+                template: PATHS.uikit + '/formElements/formElements.pug'
+            }),
+        new HtmlWebpackPlugin(
+            {
+                filename: 'roomsList/roomsList.html',
+                chunks: ['roomsList'],
+                template: PATHS.pages + 'roomsList/roomsList.pug'
+            }),
+        new HtmlWebpackPlugin(
+            {
+                filename: 'roomInfo/roomInfo.html',
+                chunks: ['roomInfo'],
+                template: PATHS.pages + 'roomInfo/roomInfo.pug'
+            }),
+        new HtmlWebpackPlugin(
+            {
+                filename: 'registration/registration.html',
+                chunks: ['registration'],
+                template: PATHS.pages + 'registration/registration.pug'
+            }),
+        new MiniCssExtractPlugin({
+            filename: '[name]/[name].css'
+        })
+    ]
 }
