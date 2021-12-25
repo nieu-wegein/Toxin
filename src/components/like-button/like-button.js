1f06933ad1cde100162abdd44146af822126eb66
Sergei <urievkan@gmail.com>
2021-12-25 22:54:20 +0300

    Сверстал like-button, отказался от единого миксина для элементов формы

---
+++ src/components/like-button/like-button.js
@@ -0,0 +1,25 @@
+import $ from "../../jquery-3.6.0.min";
+
+function toggleLike(e) {
+
+  const counter = e.currentTarget.querySelector(".like-button__counter");
+
+    if(e.currentTarget.classList.contains("like-button_active")) {
+      $(e.currentTarget).removeClass("like-button_active");
+      counter.innerText = parseInt(counter.innerText) - 1;
+    }
+    else {
+      $(e.currentTarget).addClass("like-button_active");
+      counter.innerText = parseInt(counter.innerText) + 1;
+    }
+}
+
+
+
+$(function() {
+
+  $(".like-button").each((i, button) => {
+    $(button).click(toggleLike);
+  })
+
+})
--- src/pages/UIKit/form-elements/form-elements.js
+++ src/pages/UIKit/form-elements/form-elements.js
@@ -4,6 +4,7 @@ import "../../../components/site-dropdown/site-dropdown"
 import "../../../components/site-calendar/site-calendar"
 import "../../../components/site-dropdown/site-calendarDropdown"
 import "../../../components/site-input/site-input"
+import "../../../components/like-button/like-button"
 import $ from "../../../jquery-3.6.0.min";
 
 
@@ -11,5 +12,10 @@ window.addEventListener('load', function() {
 
   const inputList = $(".site-input");
   inputList[1].value = "This is pretty awesome"
+
+  const likeButtons = $(".like-button");
+  $(likeButtons[1]).addClass("like-button_active");
+  $(".like-button__counter", likeButtons[0]).text("2");
+  $(".like-button__counter", likeButtons[1]).text("12");
 
 })
