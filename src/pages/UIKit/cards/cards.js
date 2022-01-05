58704d2d47689348b116ce1a3578a12e16c40cd2
Sergei <urievkan@gmail.com>
2022-01-05 21:45:28 +0300

    Сверстал блок бронирования комнаты

--- src/pages/UIKit/cards/cards.js
+++ src/pages/UIKit/cards/cards.js
@@ -2,3 +2,4 @@ import "../../../normalize.scss";
 import "./cards.scss";
 import "../../../components/search-block/search-block"
 import "../../../components/registration-block/registration-block"
+import "../../../components/booking-block/booking-block"
