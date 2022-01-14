b7384d684e22a2f95ae934659c10573de922c158
Sergei <urievkan@gmail.com>
2022-01-14 10:13:02 +0300

    Закончил верстку страницы room-details

--- src/pages/Website/room-details/room-details.js
+++ src/pages/Website/room-details/room-details.js
@@ -1,2 +1,4 @@
 import "../../../normalize.scss";
-import './room-details.scss'
+import './room-details.scss';
+import '../../../components/site-testimonial/site-testimonial';
+import '../../../components/booking-block/booking-block';
