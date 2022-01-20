827efa7a488bb6afd9f5d402e5ef4df2d21acc60
Sergei <urievkan@gmail.com>
2022-01-20 18:22:47 +0300

    Сделал дропдаун-меню для навигации

--- src/pages/UIKit/cards/cards.js
+++ src/pages/UIKit/cards/cards.js
@@ -5,16 +5,4 @@ import "../../../components/registration-block/registration-block"
 import "../../../components/booking-block/booking-block"
 import "../../../components/log-in-block/log-in-block"
 import "../../../components/room-card/room-card";
-import $ from "../../../jquery-3.6.0.min";
-
-
-
-
-$(function(){
-  const rateButtons = $(".rate-button").get();
-  $("[value='5 stars']", rateButtons[0]).attr("checked", true);
-  $("[value='4 stars']", rateButtons[1]).attr("checked", true);
-
-  const roomCards = $(".room-card").get();
-})
 
